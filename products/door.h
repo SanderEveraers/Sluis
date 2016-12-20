8ea5ba639559931428266f63686aee45f73b572a
vidicon <bram31f@gmail.com>
2016-12-20 14:54:18 +0100

    edit headerfiles

---
+++ products/door.h
@@ -0,0 +1,11 @@
+#ifndef DOOR_H
+#define DOOR_H
+class Door
+{
+private:
+
+public:
+
+};
+
+#endif
---
+++ products/light.h
@@ -0,0 +1,11 @@
+#ifndef Light_H
+#define Light_H
+class Light
+{
+private:
+
+public:
+
+};
+
+#endif
